b0aa28d3d0a730201b1fa39161c2aa0113bc12d6
Nikolaos Prevenios <nick_prevenios@hotmail.com>
2023-11-11 21:42:05 +0100

    Figured the js part of saving and looking after files

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// -------------- functionallity -------------- \\
+
 // form show/hide settings
 let form = document.getElementById('regform');
 let register = document.getElementById('register');
@@ -24,86 +26,117 @@ function showSearchBar() {
 
 search.addEventListener('click', showSearchBar);
 
-// database
+
+// -------------- database -------------- \\
 
 
-
-// let summaries = [];
-// let ages = [];
-// let num = 2;
-// let num2 = 3;
-
-
-// localStorage.setItem('number', num);
-
-// localStorage.setItem('number2', num2);
-
-const titles = [];
-
-let title = document.getElementById('title');
-
-localStorage.setItem('title', JSON.stringify(title.value));
-
-let rdy_title = JSON.parse(localStorage.getItem('title'));
-
-
-
-saveData(rdy_title);
-
-
-function saveData(e) {
+//function som sparar datan
+function saveData() {
     
-    titles.push(e);
+    //Här skapas ett objekt med namn 'program' 
+    let program = {
+        
+        // Objektet program har properties som läses från formulären
+        // .value läser från input texten som skrivs
+        title : document.getElementById('title').value, 
+        genre : document.getElementById('genre').value,
+        age : document.getElementById('age').value,
+    };
+
+    // här skapas en array 'programsArray' i localstorage i form av string 
+    //om det finns ingeting än och omvandlas med JSON.parse till objekt
+    // och sparas till en variable 'objArray' 
+    const objArray = JSON.parse(localStorage.getItem('programsArray')) || [];
+
+    //med hjälp av .push trycks objektet 'program' med alla sina properties till arrayn 'objArray' 
+    objArray.push(program);
+
+    //efter sparanden av objektet 'objArray' omvandlas med JSON.stringify till string
+    //och sparas till localstorage
+    localStorage.setItem('programsArray', JSON.stringify(objArray));
     
-    console.log(titles);
+    console.log(localStorage.getItem('programsArray'));
 }
 
-console.log(localStorage);
+//function som visar datan efter sökning
+function findData() {
+    if (localStorage.getItem('programsArray') === null) {
+        alert('Databas är tom!');
+    }
+    else {
+        let key = document.getElementById('key').value;
+
+        let programsArray = JSON.parse(localStorage.getItem('programsArray'));
+        
+        for (let i = 0; i < programsArray.length; i++) {
+
+            if (programsArray[i].title === key ||
+                programsArray[i].genre === key ||
+                programsArray[i].age === key) {
+                
+                document.getElementById('display').value = 'Title: ' + programsArray[i].title + ', ' + 
+                'Genre: ' + programsArray[i].genre + ', ' + 
+                'Age: ' + programsArray[i].age; 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// let title = document.getElementById('title');
-
-// localStorage.setItem('title', JSON.stringify(title.value));
-
-// let rdy_title = localStorage.getItem('title');
-
-// console.log(localStorage);
-
-
+                // console.log(document.getElementById('display').value = 'Title: ' + programsArray[i].title + ', ' + 
+                // 'Genre: ' + programsArray[i].genre + ', ' + 
+                // 'Age: ' + programsArray[i].age);
+            }
+            else {
+                document.getElementById('display').value = 'Program not found!';
+            }
+        }
+    }
+}
 
 
 let spara = document.getElementById('spara');
 
-//spara.addEventListener('click', saveData);
+spara.addEventListener('click', saveData);
+
+let sökning = document.getElementById('find');
+
+sökning.addEventListener('click', findData);
+
+
+
+
+
+
+
+
+
+
+
 
 
 //  /* Tre funktioner, en som sparar data, en som hämtar data och visar, en som rensar. */ 
 //  // Funktion som sparar data lokalt function 
 //  function saveData() { const newData = document.getElementById('title').value; 
+
 //     //hämta värdet från formulär 
 //     console.log(newData); 
+    
 //     let existingData = localStorage.getItem('savedData'); 
 //     // hämta tidigare data 
+    
 //     existingData = existingData ? JSON.parse(existingData) : []; 
 //     //om det finns data sedan innan, konvertera från JSON till objekt, annars skapa en tom array. 
+    
 //     existingData.push(newData);
 //     //lägg till data 
+    
 //     localStorage.setItem('savedData', JSON.stringify(existingData));
 //     //gör om data till sträng, spara i local 
+    
 //     alert('Data saved to local storage!'); 
 //     document.getElementById('title').value = ''; // Clear input field 
 //     } 
+
+
+
+
 //  // Visa sparad data 
 //  function showData() { const savedData = localStorage.getItem('savedData'); 
 //     if (savedData) { 
